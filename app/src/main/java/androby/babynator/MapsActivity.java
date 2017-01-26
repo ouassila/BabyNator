e7f14daa0af7ae31edde7d8adebe42c31d590adf
Ouassila <ouassilan@yahoo.fr>
2017-01-26 14:39:22 +0100

    modif type carte

--- app/src/main/java/androby/babynator/MapsActivity.java
+++ app/src/main/java/androby/babynator/MapsActivity.java
@@ -50,7 +50,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //User has previously accepted this permission
