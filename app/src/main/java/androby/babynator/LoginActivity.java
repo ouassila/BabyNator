97fde3b3acc792bfa3d53aad9e73b066c786929e
Kevin <deveaukevin77@gmail.com>
2017-01-26 13:51:10 +0100

    ajout logo

--- app/src/main/java/androby/babynator/LoginActivity.java
+++ app/src/main/java/androby/babynator/LoginActivity.java
@@ -82,6 +82,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
