222aade845afbda6b6a0ffb01841ea5f972853c1
Ouassila <ouassilan@yahoo.fr>
2017-01-29 16:30:14 +0100

    modif calendar ajout formulaire event

--- app/src/main/java/androby/babynator/AddEventActivity.java
+++ app/src/main/java/androby/babynator/AddEventActivity.java
@@ -1,13 +1,22 @@
 package androby.babynator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class AddEventActivity extends AppCompatActivity {
+
+    private String date;
+    private TextView lbl_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+        date = getIntent().getStringExtra("DATE");
+        lbl_date = (TextView) findViewById(R.id.lbl_date);
+        lbl_date.setText(date);
+
     }
 }
--- app/src/main/java/androby/babynator/CalendarActivity.java
+++ app/src/main/java/androby/babynator/CalendarActivity.java
@@ -1,20 +1,21 @@
 package androby.babynator;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.DatePicker;
 
+import java.util.Calendar;
 import java.util.Locale;
 
 public class CalendarActivity extends AppCompatActivity {
 
-    DatePicker calendar;
+    DatePicker mDatePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
 
         //mettre en francais
         String languageToLoad  = "fr";
@@ -25,5 +26,21 @@ public class CalendarActivity extends AppCompatActivity {
         getBaseContext().getResources().updateConfiguration(config,
                 getBaseContext().getResources().getDisplayMetrics());
         this.setContentView(R.layout.activity_calendar);
+
+        mDatePicker = (DatePicker) findViewById(R.id.datePicker);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        //define min max
+        mDatePicker.setMinDate(calendar.getTimeInMillis());
+
+        mDatePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Intent myIntent = new Intent(CalendarActivity.this, AddEventActivity.class);
+                myIntent.putExtra("DATE", dayOfMonth+"/"+ (month+1) +"/"+year);
+                startActivity(myIntent);
+            }
+        });
     }
 }
