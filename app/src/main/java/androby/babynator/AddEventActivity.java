edad8cfaaeded4425e909ede5da0bbf59364d327
Ouassila <ouassilan@yahoo.fr>
2017-01-30 10:32:02 +0100

    ajout titre add event

--- app/src/main/java/androby/babynator/AddEventActivity.java
+++ app/src/main/java/androby/babynator/AddEventActivity.java
@@ -1,13 +1,21 @@
 package androby.babynator;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class AddEventActivity extends AppCompatActivity {
 
     private String date;
     private TextView lbl_date;
+    private EditText hour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,26 @@ public class AddEventActivity extends AppCompatActivity {
         date = getIntent().getStringExtra("DATE");
         lbl_date = (TextView) findViewById(R.id.lbl_date);
         lbl_date.setText(date);
+
+        hour = (EditText) findViewById(R.id.hour);
+        hour.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(AddEventActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        AddEventActivity.this.hour.setText(selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Séléctionnez l'heure ");
+                mTimePicker.show();
+            }
+        });
 
     }
 }
--- app/src/main/java/androby/babynator/CalendarActivity.java
+++ app/src/main/java/androby/babynator/CalendarActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.DatePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 public class CalendarActivity extends AppCompatActivity {
 
-    DatePicker mDatePicker;
+    private DatePicker mDatePicker;
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +30,27 @@ public class CalendarActivity extends AppCompatActivity {
         this.setContentView(R.layout.activity_calendar);
 
         mDatePicker = (DatePicker) findViewById(R.id.datePicker);
-        Calendar calendar = Calendar.getInstance();
+        calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
         //define min max
         mDatePicker.setMinDate(calendar.getTimeInMillis());
-
         mDatePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
 
             @Override
             public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+
                 Intent myIntent = new Intent(CalendarActivity.this, AddEventActivity.class);
-                myIntent.putExtra("DATE", dayOfMonth+"/"+ (month+1) +"/"+year);
+                myIntent.putExtra("DATE", dayOfMonth+" "+nameMonth(month) +" "+year);
                 startActivity(myIntent);
             }
         });
+    }
+
+    private String nameMonth(int number){
+
+        Calendar cal=Calendar.getInstance();
+        SimpleDateFormat month_date = new SimpleDateFormat("MMMM");
+        cal.set(Calendar.MONTH, (number));
+        return month_date.format(cal.getTime()).toUpperCase();
     }
 }
