4dc6c5456d694bcef5f2019a8042d98ae8fe9993
Ouassila <ouassilan@yahoo.fr>
2017-02-13 09:35:05 +0100

    loading page

--- app/src/main/java/androby/babynator/LoginActivity.java
+++ app/src/main/java/androby/babynator/LoginActivity.java
@@ -31,8 +31,11 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.jpardogo.android.googleprogressbar.library.FoldingCirclesDrawable;
 
 import org.json.JSONObject;
 
@@ -70,6 +73,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private ProgressBar mProgressBar;
 
     public static String IP_SERVER = "192.168.43.10:8080";
 
@@ -117,8 +121,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailRegister.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
                 Intent myIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(myIntent);
             }
@@ -126,6 +128,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.login_progress);
+        mProgressBar.setIndeterminateDrawable(new FoldingCirclesDrawable.Builder(this)
+                .build());
+
         View view = this.getCurrentFocus();
         if (view != null) {
             InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -176,7 +183,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         }
     }
-
 
     /**
      * Attempts to sign in or register the account specified by the login form.
@@ -248,18 +254,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
         // for very easy animations. If available, use these APIs to fade-in
         // the progress spinner.
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
             mProgressView.animate().setDuration(shortAnimTime).alpha(
                     show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
@@ -269,10 +266,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 }
             });
         } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
         }
     }
 
